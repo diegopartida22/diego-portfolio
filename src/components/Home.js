fe2f6d40f01b39ec16ac04f71cd61aa8821ede46
diegopartida22 <A01641113@tec.mx>
2023-05-26 16:10:28 -0600

    Edit Navbar

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,6 +6,11 @@ function Home() {
       <h1 className="text-6xl font-bold text-gray-800 flex justify-center">
         Hi! I'm Diego Partida
       </h1>
+
+      <p className="text-2xl text-gray-600 flex justify-center mt-8">
+        I'm a full-stack web developer and a student at Tecnológico de
+        Monterrey.
+      </p>
     </div>
   );
 }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,114 +2,154 @@ import { useState } from "react";
 import "./NavBar.css";
 
 function NavBar() {
+  const [showNav, setShowNav] = useState(false);
+  const [showMenuToggle, setShowMenuToggle] = useState(false);
   const [showMenu, setShowMenu] = useState(false);
-  const [hoverOption, setHoverOption] = useState(false);
 
   return (
-    <div className="font-bold text-lg flex justify-end relative">
-      <button
-        className="text-gray-300 bg-gray-800 hover:bg-gray-600 rounded-md p-4 m-4 transition duration-500 ease-in-out"
-        onClick={() => setShowMenu(!showMenu)}
-      >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          strokeWidth={1.5}
-          stroke="currentColor"
-          className="w-10 h-8"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            d="M3.75 9h16.5m-16.5 6.75h16.5"
-          />
-        </svg>
-      </button>
+    <div className="pb-6">
+      <div className=" font-bold text-lg flex justify-center p-6 shadow-md">
+        <ul className="flex flex-row gap-6">
+          <li>
+            <a
+              href="/"
+              className="text-gray-900 hover:font-bold hover:text-gray-400"
+            >
+              Home
+            </a>
+          </li>
+          <li>
+            <a
+              href="/about"
+              className="text-gray-900 hover:font-bold hover:text-gray-400"
+            >
+              About
+            </a>
+          </li>
+          <li>
+            <a
+              href="/projects"
+              className="text-gray-900 hover:font-bold hover:text-gray-400"
+            >
+              Projects
+            </a>
+          </li>
+          <li>
+            <a
+              href="/contact"
+              className="text-gray-900 hover:font-bold hover:text-gray-400"
+            >
+              Contact
+            </a>
+          </li>
+        </ul>
+      </div>
 
-      {showMenu && (
-        <div className="absolute mt-24 mr-4 bg-gray-300 p-4 w-1/6 rounded-md shadow-lg animate-slide-down">
-          <ul className="flex flex-col gap-6 ml-8">
-            <li className="flex justify-end">
-              <button
-                className="text-gray-900 hover:text-indigo-400"
-                onClick={() => setShowMenu(!showMenu)}
-              >
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth={1.5}
-                  stroke="currentColor"
-                  className="w-10 h-10 hover:rotate-180 transition duration-500 ease-in-out"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M6 18L18 6M6 6l12 12"
-                  />
-                </svg>
-              </button>
-            </li>
-            <li>
-              <a
-                href="/"
-                className="text-gray-900 hover:font-bold hover:text-gray-400"
-                onMouseDown={() => setHoverOption(true)}
-                onMouseUp={() => setHoverOption(false)}
-              >
-                Home
-              </a>
-            </li>
-            <li>
-              <a
-                href="/about"
-                className="text-gray-900 hover:font-bold hover:text-gray-400"
-              >
-                About
-              </a>
-            </li>
-            <li>
-              <a
-                href="/projects"
-                className="text-gray-900 hover:font-bold hover:text-gray-400"
-              >
-                Projects
-              </a>
-            </li>
-            <li>
-              <a
-                href="/contact"
-                className="text-gray-900 hover:font-bold hover:text-gray-400"
-              >
-                Contact
-              </a>
-            </li>
+      {showMenuToggle && (
+        <div className="font-bold text-lg flex justify-end relative">
+          <button
+            className="text-gray-300 bg-gray-800 hover:bg-gray-600 rounded-md p-4 m-4 transition duration-500 ease-in-out"
+            onClick={() => setShowMenu(!showMenu)}
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="w-10 h-8"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M3.75 9h16.5m-16.5 6.75h16.5"
+              />
+            </svg>
+          </button>
 
-            <li className="mb-10">
-              <a
-                href="./assets/DiegoPartida_Resume.pdf"
-                download="DiegoPartida_Resume.pdf"
-                className="text-gray-900 hover:font-bold hover:text-gray-400 flex flex-row gap-2"
-              >
-                CV
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth={1.5}
-                  stroke="currentColor"
-                  className="w-6 h-6"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5M16.5 12L12 16.5m0 0L7.5 12m4.5 4.5V3"
-                  />
-                </svg>
-              </a>
-            </li>
-          </ul>
+          {showMenu && (
+            <div className="absolute mt-24 mr-4 bg-gray-300 p-4 w-1/6 rounded-md shadow-lg animate-slide-down">
+              <ul className="flex flex-col gap-6 ml-8">
+                <li className="flex justify-end">
+                  <button
+                    className="text-gray-900 hover:text-indigo-400"
+                    onClick={() => setShowMenu(!showMenu)}
+                  >
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      strokeWidth={1.5}
+                      stroke="currentColor"
+                      className="w-10 h-10 hover:rotate-180 transition duration-500 ease-in-out"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M6 18L18 6M6 6l12 12"
+                      />
+                    </svg>
+                  </button>
+                </li>
+                <li>
+                  <a
+                    href="/"
+                    className="text-gray-900 hover:font-bold hover:text-gray-400"
+                  >
+                    Home
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="/about"
+                    className="text-gray-900 hover:font-bold hover:text-gray-400"
+                  >
+                    About
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="/projects"
+                    className="text-gray-900 hover:font-bold hover:text-gray-400"
+                  >
+                    Projects
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="/contact"
+                    className="text-gray-900 hover:font-bold hover:text-gray-400"
+                  >
+                    Contact
+                  </a>
+                </li>
+
+                <li className="mb-10">
+                  <a
+                    href="./assets/DiegoPartida_Resume.pdf"
+                    download="DiegoPartida_Resume.pdf"
+                    className="text-gray-900 hover:font-bold hover:text-gray-400 flex flex-row gap-2"
+                  >
+                    CV
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      strokeWidth={1.5}
+                      stroke="currentColor"
+                      className="w-6 h-6"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5M16.5 12L12 16.5m0 0L7.5 12m4.5 4.5V3"
+                      />
+                    </svg>
+                  </a>
+                </li>
+              </ul>
+            </div>
+          )}
         </div>
       )}
     </div>
