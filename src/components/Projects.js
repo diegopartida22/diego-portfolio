9b3e24b1161d877de0eb581aa419065c10f5881c
diegopartida22 <A01641113@tec.mx>
2023-05-28 20:02:02 -0600

    Edit the project section

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -6,37 +6,37 @@ function Projects() {
       title: "ApeironAI",
       description:
         "Developed a range of tools for creative and practical applications such as coding, marketing, art, and typography.",
-      imageUrl: "/images/ApeironAI.PNG",
+      imageUrl: "/images/projects/ApeironAI.PNG",
     },
     {
       title: "Numerologist Portfolio",
       description:
         "Technical skills in React.js and Bootstrap, with a deep understanding of front-end development best practices.",
-      imageUrl: "/images/NumerologistPortfolio.PNG",
+      imageUrl: "/images/projects/MarychelyPortfolio.png",
     },
-    {
-      title: "Diagnosis of malaria through a red blood cell",
-      description:
-        "Developed a MatLab App for malaria diagnosis using non-uniform electric fields to analyze red blood cells.",
-      imageUrl: "/images/MalariaDiagnosis.PNG",
-    },
-    {
-      title: "Phylogenetic tree of SARS-COV-2 variants in different countries",
-      description:
-        "Created a phylogenetic tree of SARS-COV-2 variants in different countries using RStudio program and NCBI database.",
-      imageUrl: "/images/SARS-COV-2PhylogeneticTree.PNG",
-    },
+    // {
+    //   title: "Diagnosis of malaria through a red blood cell",
+    //   description:
+    //     "Developed a MatLab App for malaria diagnosis using non-uniform electric fields to analyze red blood cells.",
+    //   imageUrl: "/images/MalariaDiagnosis.PNG",
+    // },
+    // {
+    //   title: "Phylogenetic tree of SARS-COV-2 variants in different countries",
+    //   description:
+    //     "Created a phylogenetic tree of SARS-COV-2 variants in different countries using RStudio program and NCBI database.",
+    //   imageUrl: "/images/SARS-COV-2PhylogeneticTree.PNG",
+    // },
     {
       title: "Scheduling Web-App",
       description:
         "Developed an automated scheduling system that makes it easier to manage your daily tasks.",
-      imageUrl: "/images/SchedulingWebApp.PNG",
+      imageUrl: "/images/projects/SchedulingApp.PNG",
     },
     {
       title: "My Portfolio",
       description:
         "This is my portfolio, where I show my projects and my skills.",
-      imageUrl: "/images/MyPortfoliox.PNG",
+      imageUrl: "/images/projects/MyPortfolio.png",
     },
   ];
 
@@ -48,13 +48,13 @@ function Projects() {
         Projects
       </h1>
 
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-8 mt-8 px-4">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 gap-10 mt-8 px-8">
         {projects.map((project, index) => (
           <div key={index} className="bg-white rounded-lg shadow-lg">
             <img
               src={project.imageUrl}
               alt={project.title}
-              className="w-full h-48 object-cover object-center rounded-t-lg"
+              className="w-full  object-cover object-center rounded-t-lg"
             />
             <div className="p-6">
               <h2 className="text-2xl font-bold text-gray-800 mb-2">
