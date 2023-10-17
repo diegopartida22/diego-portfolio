2632eb23fcbedca4dd7f178fbe849b2316e0b74e
diegopartida22 <a01641113@tec.mx>
2023-10-17 09:27:26 -0600

    Fix errors

--- src/components/Habilities.js
+++ src/components/Habilities.js
@@ -73,10 +73,10 @@ function Habilities() {
 
             <button
               onClick={() => handleCategoryChange("frameworks")}
-              class="relative inline-flex items-center justify-center px-6 py-3 text-lg tracking-tighter text-white bg-gray-800 rounded-md group"
+              className="relative inline-flex items-center justify-center px-6 py-3 text-lg tracking-tighter text-white bg-gray-800 rounded-md group"
             >
               <span
-                class={`absolute inset-0 w-full h-full mt-1 ml-1 transition-all duration-300 ease-in-out bg-gray-800 rounded-md group-hover:mt-0 group-hover:ml-0 ${
+                className={`absolute inset-0 w-full h-full mt-1 ml-1 transition-all duration-300 ease-in-out bg-gray-800 rounded-md group-hover:mt-0 group-hover:ml-0 ${
                   currentCategory === "frameworks" ? "opacity-0" : ""
                 }`}
               ></span>
@@ -171,9 +171,9 @@ function Habilities() {
                     : "bg-gray-200"
                 }`}
               ></span>
-              <span class="absolute inset-0 w-full h-full transition-all duration-200 ease-in-out delay-100 bg-gray-800 rounded-md opacity-0 group-hover:opacity-100 "></span>
+              <span className="absolute inset-0 w-full h-full transition-all duration-200 ease-in-out delay-100 bg-gray-800 rounded-md opacity-0 group-hover:opacity-100 "></span>
               <span
-                class={`relative text-gray-900 transition-colors duration-200 ease-in-out delay-100 group-hover:text-white ${
+                className={`relative text-gray-900 transition-colors duration-200 ease-in-out delay-100 group-hover:text-white ${
                   currentCategory === "technologies" ? "text-white" : ""
                 }`}
               >
