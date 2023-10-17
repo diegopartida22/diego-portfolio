17598a36998d539a79354bbe77e6b8b4c26bec51
diegopartida22 <a01641113@tec.mx>
2023-10-17 09:17:37 -0600

    Add navbar blog

---
+++ src/components/NavBarBlog.js
@@ -0,0 +1,96 @@
+import { useState, useEffect } from "react";
+import { Link as RouterLink } from "react-router-dom";
+import "./NavBar.css";
+
+function NavBarBlog() {
+  const [showNavbar, setShowNavbar] = useState(true);
+  const [showMenuToggle, setShowMenuToggle] = useState(false);
+  const [showMenu, setShowMenu] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollTop = window.pageYOffset;
+
+      if (scrollTop > 0) {
+        setShowNavbar(false);
+        setShowMenuToggle(true);
+      } else {
+        setShowNavbar(true);
+        setShowMenuToggle(false);
+        setShowMenu(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  return (
+    <div id="navbar">
+      <div
+        className={`font-bold text-s sm:text-2xl flex justify-center p-6 mb-6 shadow-md fixed top-0 w-full bg-white z-10  transition-all duration-500 ${
+          showNavbar ? "opacity-100 animate-slide-up" : "opacity-0"
+        }`}
+      >
+        <ul className="flex flex-row gap-6 group">
+          <li>
+            <RouterLink
+              className="text-gray-700 font-bold hover:text-orange-500 cursor-pointer flex flex-row gap-2 items-center"
+              to="/"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                class="w-6 h-6"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25"
+                />
+              </svg>
+              Home
+            </RouterLink>
+          </li>
+        </ul>
+      </div>
+      <div
+        className={`font-bold text-lg sm:p-6 mb-6 fixed top-0 right-0 z-20 transition-all duration-200 ${
+          showMenuToggle
+            ? "animate-slide-left opacity-100 "
+            : "animate-slide-right opacity-0"
+        }`}
+      >
+        <RouterLink to="/">
+          <button
+            className="text-gray-300 bg-gray-800 hover:bg-gray-600 rounded-md p-2 sm:p-4 m-4 transition duration-200 ease-in-out"
+            onClick={() => setShowMenu(!showMenu)}
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="currentColor"
+              class="w-8 h-8"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25"
+              />
+            </svg>
+          </button>
+        </RouterLink>
+      </div>
+    </div>
+  );
+}
+
+export default NavBarBlog;
--- src/pages/Blog.js
+++ src/pages/Blog.js
@@ -1,107 +1,112 @@
 import React from "react";
-import NavBar from "../components/NavBar";
+import NavBarBlog from "../components/NavBarBlog";
+import Footer from "../components/Footer";
 
 function Blog() {
   return (
     <div className="mt-32 flex justify-center flex-col" id="blog">
-      <NavBar />
-      <h1 className="text-3xl sm:text-6xl font-bold text-gray-800 text-center">
-        Blog
-      </h1>
+      <NavBarBlog />
+      <div className="w-3/4 mx-auto">
+        <h1 className="text-3xl sm:text-6xl font-bold text-gray-800 text-center">
+          Blog
+        </h1>
 
-      <div className="p-10 sm:px-20 text-gray-700 text-lg">
-        <h2 className="text-2xl font-bold text-gray-800">
-          A personal view of the history of testing in software systems
-        </h2>
+        <div className="p-10 sm:px-20 text-gray-700 text-lg">
+          <h2 className="text-2xl font-bold text-gray-800">
+            A personal view of the history of testing in software systems
+          </h2>
 
-        <p className="mt-4">
-          Software testing has come a long way since its humble beginnings in
-          the mid-20th century. In the early days, testing was largely manual
-          and ad-hoc, with developers simply using the software themselves to
-          try to find bugs. As software became more complex, however, it became
-          clear that more rigorous and systematic testing was needed.
-        </p>
+          <p className="mt-4">
+            Software testing has come a long way since its humble beginnings in
+            the mid-20th century. In the early days, testing was largely manual
+            and ad-hoc, with developers simply using the software themselves to
+            try to find bugs. As software became more complex, however, it
+            became clear that more rigorous and systematic testing was needed.
+          </p>
 
-        <p className="mt-4">
-          In the 1970s and 1980s, a number of new testing methodologies and
-          tools were developed, such as black-box testing, white-box testing,
-          and regression testing. These methodologies provided a more systematic
-          approach to testing, and the new tools helped to automate many of the
-          repetitive tasks involved.
-        </p>
+          <p className="mt-4">
+            In the 1970s and 1980s, a number of new testing methodologies and
+            tools were developed, such as black-box testing, white-box testing,
+            and regression testing. These methodologies provided a more
+            systematic approach to testing, and the new tools helped to automate
+            many of the repetitive tasks involved.
+          </p>
 
-        <p className="mt-4">
-          In the 1990s, software testing became increasingly integrated with the
-          software development lifecycle (SDLC). This led to the emergence of
-          new concepts such as continuous testing and quality assurance (QA).
-          Continuous testing involves testing software throughout the
-          development process, rather than just at the end. QA is a broader
-          discipline that encompasses all aspects of software quality, including
-          testing, requirements management, and risk management.
-        </p>
+          <p className="mt-4">
+            In the 1990s, software testing became increasingly integrated with
+            the software development lifecycle (SDLC). This led to the emergence
+            of new concepts such as continuous testing and quality assurance
+            (QA). Continuous testing involves testing software throughout the
+            development process, rather than just at the end. QA is a broader
+            discipline that encompasses all aspects of software quality,
+            including testing, requirements management, and risk management.
+          </p>
 
-        <p className="mt-4">
-          In recent years, software testing has continued to evolve, with new
-          technologies and trends emerging all the time. For example, the rise
-          of DevOps has led to a greater focus on automated testing and
-          continuous integration and continuous delivery (CI/CD). Artificial
-          intelligence (AI) is also beginning to play a role in software
-          testing, with new tools and techniques being developed to help testers
-          automate tasks such as test case generation and defect detection.
-        </p>
+          <p className="mt-4">
+            In recent years, software testing has continued to evolve, with new
+            technologies and trends emerging all the time. For example, the rise
+            of DevOps has led to a greater focus on automated testing and
+            continuous integration and continuous delivery (CI/CD). Artificial
+            intelligence (AI) is also beginning to play a role in software
+            testing, with new tools and techniques being developed to help
+            testers automate tasks such as test case generation and defect
+            detection.
+          </p>
 
-        <h3 className="text-2xl font-bold text-gray-800 mt-10">Conclusion</h3>
+          <h3 className="text-2xl font-bold text-gray-800 mt-10">Conclusion</h3>
 
-        <p className="mt-4">
-          Software testing has come a long way since its early days. Today, it
-          is a mature and well-established discipline with a wide range of
-          methodologies and tools available. Software testing is essential for
-          ensuring the quality of software, and it plays a vital role in the
-          software development lifecycle.
-        </p>
+          <p className="mt-4">
+            Software testing has come a long way since its early days. Today, it
+            is a mature and well-established discipline with a wide range of
+            methodologies and tools available. Software testing is essential for
+            ensuring the quality of software, and it plays a vital role in the
+            software development lifecycle.
+          </p>
 
-        <p className="mt-4">
-          I am excited about the future of software testing, and I believe that
-          it will continue to evolve and innovate as software becomes
-          increasingly complex and critical to our everyday lives.
-        </p>
+          <p className="mt-4">
+            I am excited about the future of software testing, and I believe
+            that it will continue to evolve and innovate as software becomes
+            increasingly complex and critical to our everyday lives.
+          </p>
+        </div>
+
+        <div className="p-10 sm:px-20 text-gray-700 text-lg">
+          References:
+          <ul className="list-disc list-inside pl-1">
+            <li>
+              <a
+                href="https://www.geeksforgeeks.org/history-of-software-testing/"
+                target="_blank"
+                rel="noreferrer"
+                className="text-blue-500 hover:text-blue-700"
+              >
+                https://www.geeksforgeeks.org/history-of-software-testing/
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://dogq.io/blog/a-brief-history-of-software-testing/"
+                target="_blank"
+                rel="noreferrer"
+                className="text-blue-500 hover:text-blue-700"
+              >
+                https://dogq.io/blog/a-brief-history-of-software-testing/
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://www.softwaretestinghelp.com/what-is-software-testing/"
+                target="_blank"
+                rel="noreferrer"
+                className="text-blue-500 hover:text-blue-700"
+              >
+                https://www.softwaretestinghelp.com/what-is-software-testing/
+              </a>
+            </li>
+          </ul>
+        </div>
       </div>
-
-      <div className="p-10 sm:px-20 text-gray-700 text-lg">
-        References:
-        <ul className="list-disc list-inside pl-1">
-          <li>
-            <a
-              href="https://www.geeksforgeeks.org/history-of-software-testing/"
-              target="_blank"
-              rel="noreferrer"
-              className="text-blue-500 hover:text-blue-700"
-            >
-              https://www.geeksforgeeks.org/history-of-software-testing/
-            </a>
-          </li>
-          <li>
-            <a
-              href="https://dogq.io/blog/a-brief-history-of-software-testing/"
-              target="_blank"
-              rel="noreferrer"
-              className="text-blue-500 hover:text-blue-700"
-            >
-              https://dogq.io/blog/a-brief-history-of-software-testing/
-            </a>
-          </li>
-          <li>
-            <a
-              href="https://www.softwaretestinghelp.com/what-is-software-testing/"
-              target="_blank"
-              rel="noreferrer"
-              className="text-blue-500 hover:text-blue-700"
-            >
-              https://www.softwaretestinghelp.com/what-is-software-testing/
-            </a>
-          </li>
-        </ul>
-      </div>
+      <Footer />
     </div>
   );
 }
